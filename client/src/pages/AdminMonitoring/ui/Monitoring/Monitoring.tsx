e364630aaa10bdc55a3d83f11ded1c902fb8eee9
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-02-25 17:54:51 +0300

    stats row component

--- client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
+++ client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
@@ -1,11 +1,12 @@
 import styles from './Monitoring.module.css';
 import { PeriodSwitcher } from './PeriodSwitcher/PeriodSwitcher';
-
+import { StatsRow } from './StatsRow/StatsRow';
 
 export const Monitoring = () => {
   return (
     <div className={styles.monitoring}>
       <PeriodSwitcher />
+      <StatsRow />
     </div>
   );
 };
---
+++ client/src/pages/AdminMonitoring/ui/Monitoring/StatsRow/StatsRow.tsx
@@ -0,0 +1,78 @@
+import ArrowIcon from '../../../assets/arrowRight.svg?react';
+import styles from './StatsRow.module.css';
+
+type StatStatus = 'good' | 'warning' | 'danger' | null;
+
+type Stat = {
+  label: string;
+  value: number;
+  percent?: string;
+  status?: StatStatus;
+  showArrow?: boolean;
+};
+
+const statsData: Stat[] = [
+  {
+    label: 'Всего автоматов',
+    value: 78,
+    showArrow: true,
+  },
+  {
+    label: 'Работающих',
+    value: 76,
+    percent: '98%',
+    status: 'good',
+    showArrow: true,
+  },
+  {
+    label: 'Мало товаров',
+    value: 16,
+    percent: '12%',
+    status: 'warning',
+    showArrow: true,
+  },
+  {
+    label: 'Требуют обслуживания',
+    value: 2,
+    percent: '1%',
+    status: 'danger',
+    showArrow: false,
+  },
+];
+
+export const StatsRow = () => {
+  return (
+    <div className={styles.statsRow}>
+      {statsData.map((item, index) => (
+        <div key={index} className={styles.statItem}>
+          <div className={styles.label}>
+            <span>{item.label}</span>
+            {item.showArrow && <ArrowIcon className={styles.arrow} />}
+          </div>
+
+          <div className={styles.valueWrapper}>
+            <span
+              className={
+                item.status
+                  ? `${styles.value} ${styles[item.status]}`
+                  : styles.value
+              }
+            >
+              {item.value}
+            </span>
+
+            {item.percent && (
+              <span
+                className={
+                  item.status ? `${styles.percent} ${styles[item.status]}` : styles.percent
+                }
+              >
+                {item.percent}
+              </span>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
