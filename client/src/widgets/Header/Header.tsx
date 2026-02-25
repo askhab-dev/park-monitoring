d415de2c4bbc55e14edfad4a4be5d76bf1965459
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-02-25 11:55:16 +0300

    LastUpdate component

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -3,6 +3,7 @@ import { ParksNavigation } from './ui/ParksNavigation/ParksNavigation'
 import { LogoLink } from './ui/LogoLink/LogoLink'
 import { parks } from './config/parks'
 import { Search } from './ui/Search/Search';
+import { LastUpdate } from './ui/LastUpdate/LastUpdate';
 
 export const Header: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ export const Header: React.FC = () => {
       <LogoLink />
       <ParksNavigation parks={parks} />
       <Search />
+      <LastUpdate />
     </div>
   )
 }
---
+++ client/src/widgets/Header/ui/LastUpdate/LastUpdate.tsx
@@ -0,0 +1,11 @@
+import StatusIcon from '../../assets/status.svg?react';
+import styles from './LastUpdate.module.css'
+
+export const LastUpdate: React.FC = () => {
+  return (
+    <div className={styles.lastUpdate}>
+      <StatusIcon className={styles.statusIcon} />
+      Обновлено<br/>20.09.2024 в 12:35
+    </div>
+  )
+};
