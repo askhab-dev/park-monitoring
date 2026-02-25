62098841f9259bee4cc050d8f759cf7a25661bf9
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-02-25 19:08:52 +0300

    VM params switcher

--- client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
+++ client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
@@ -3,13 +3,33 @@ import { StatsRow } from './StatsRow/StatsRow';
 import { Map } from './Map/Map';
 
 import styles from './Monitoring.module.css';
+import { MultiSwitcher } from '@/shared/ui/MultiSwitcher/MultiSwitcher';
+import { useState } from 'react';
+
+type VMParam = 'state' | 'revenue' | 'downtime' | 'fillLevel';
+
+const VMParams: { id: VMParam; label: string }[] = [
+  { id: 'state', label: 'Состояние автоматов' },
+  { id: 'revenue', label: 'Средняя выручка' },
+  { id: 'downtime', label: 'Простой ТА' },
+  { id: 'fillLevel', label: 'Уровень заполнения' },
+];
+
 
 export const Monitoring = () => {
+    const [active, setActive] = useState<VMParam>('state');
+  
   return (
     <div className={styles.monitoring}>
       <PeriodSwitcher />
       <StatsRow />
       <Map />
+      <MultiSwitcher
+        className={styles.vmSwitcher}
+        items={VMParams}
+        value={active}
+        onChange={setActive}
+      />
     </div>
   );
 };
