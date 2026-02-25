ca91aae1731204ba00a5925af70d54a951935254
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-02-25 18:51:04 +0300

    map components

---
+++ client/src/pages/AdminMonitoring/ui/Monitoring/Map/Map.tsx
@@ -0,0 +1,11 @@
+import styles from './Map.module.css';
+
+const YA_MAP_LINK = 'https://yandex.ru/map-widget/v1/?ll=37.592023%2C55.704891&mode=whatshere&whatshere%5Bpoint%5D=37.591108%2C55.706946&whatshere%5Bzoom%5D=15.91&z=15.91';
+
+export const Map = () => {
+  return (
+    <div className={styles.map}>
+        <iframe src={YA_MAP_LINK} allowFullScreen={true} />
+      </div>
+  );
+};
--- client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
+++ client/src/pages/AdminMonitoring/ui/Monitoring/Monitoring.tsx
@@ -1,12 +1,15 @@
-import styles from './Monitoring.module.css';
 import { PeriodSwitcher } from './PeriodSwitcher/PeriodSwitcher';
 import { StatsRow } from './StatsRow/StatsRow';
+import { Map } from './Map/Map';
+
+import styles from './Monitoring.module.css';
 
 export const Monitoring = () => {
   return (
     <div className={styles.monitoring}>
       <PeriodSwitcher />
       <StatsRow />
+      <Map />
     </div>
   );
 };
